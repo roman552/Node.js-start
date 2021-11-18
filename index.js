a983c53327e97ce9386a8cda07ed5928cb382013
roman552 <roman.ivanov@ivkhk.ee>
2021-11-18 10:00:20 +0200

    8. Simple Web-server

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 const http = require("http")
 
 const server = http.createServer((req, res) => {
-    console.log(req.url)
+    
+    if(req.method === 'GET'){
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        })
+    
+        res.end(`
+        <h1>Form</h1>
+        <form method="post" action="/">
+            <input name="title" type="text" />
+            <button type="submit">Send</button>
+        </form>
+        `)
+    } else if (req.method === 'POST') {
+        const body = []
+        res.writeHead(200, {
+            'Content-type': 'text/html; charset=utf-8'
+        })
 
-    res.write("<h1>Hello from NodeJS</h1>")
-    res.write("<h2>Hello from NodeJS</h2>")
-    res.write("<h3>Hello from NodeJS</h3>")
-    res.end(`
-        <div style="background: red; width: 200px; height: 200px;">
-            <h1>Test</h1>
-        </div>
-    `)
+        req.on('data', data => {
+            body.push(Buffer.from(data))
+        })
+
+        req.on('end',() => {
+            const message = body.toString().split('=')[1]
+
+            res.end(`
+                <h1>Ваше сообщение: ${message}</h1>
+            `)
+        })
+    }
 })
 
 server.listen(3000, () => {
